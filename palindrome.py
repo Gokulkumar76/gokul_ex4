28b694e55127a008d16d1706f4818edb9340dacc
DHAMUUU <dhamodharanr@karunya.edu.in>
2025-04-01 21:14:36 +0530

    #1 fix

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,13 @@
+#initializing variable
 num = 1221
 temp = num
 reverse = 0
+#logic
 while temp > 0:
     remainder = temp % 10
     reverse = (reverse * 10) + remainder
     temp = temp // 10
+#printing result
 if num == reverse:
   print('Palindrome')
 else:
